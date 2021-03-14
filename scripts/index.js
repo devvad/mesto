5106e38c364f4ffdc3b77736a9669e36257ec976
Vadim Cherepanov <vadim.rgsu@yandex.ru>
2021-03-14 17:37:06 +0300

    Исправил часть ошибок

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,18 @@
 // 0 общие функции для попапов
 function makePopup(rootElement) {
-	const popup = {
-		open() {
-			rootElement.classList.add("popup_opened");
-		},
-		close() {
-			rootElement.classList.remove("popup_opened");
-		}
-	};
-	const closeButtonElement = rootElement.querySelector(".popup__closed");
-	closeButtonElement.addEventListener("click", function() {
-		popup.close();
-	});
-	return popup;
+  const popup = {
+    open() {
+      rootElement.classList.add("popup_opened");
+    },
+    close() {
+      rootElement.classList.remove("popup_opened");
+    }
+  };
+  const closeButtonElement = rootElement.querySelector(".popup__closed");
+  closeButtonElement.addEventListener("click", function() {
+    popup.close();
+  });
+  return popup;
 }
 
 // 1 попап - Редактирование профиля:
@@ -23,7 +23,7 @@ const titleInput = document.querySelector("#input-popup-title");
 const subtitleInput = document.querySelector("#input-popup-subtitle");
 const titleProfile = document.querySelector(".profile__title");
 const subtitleProfile = document.querySelector(".profile__subtitle");
-const form = document.querySelector(".popup__form");
+const editProfileForm = document.querySelector(".popup__form");
 
 editButton.addEventListener("click", function() {
 	editPopup.open();
@@ -31,13 +31,13 @@ editButton.addEventListener("click", function() {
 	subtitleInput.value = subtitleProfile.textContent;
 });
 
-function formSubmitHandler(evt) {
+function submitEditProfileForm(evt) {
 	evt.preventDefault();
 	titleProfile.textContent = titleInput.value;
 	subtitleProfile.textContent = subtitleInput.value;
 	editPopup.close();
 }
-form.addEventListener("submit", formSubmitHandler);
+editProfileForm.addEventListener("submit", submitEditProfileForm);
 
 // 2 попап - Добавление нового места:
 const addPopup = makePopup(document.querySelector(".popup_type_place"));
@@ -47,15 +47,12 @@ addButton.addEventListener("click", function() {
 	addPopup.open();
 });
 
-const newCardForm = document.querySelector(".popup_type_place .popup__form");
+const newCardForm = document.querySelector(".popup__form-add-card");
+const nameInput = document.querySelector(".popup__input_type_name");
+const imageInput = document.querySelector(".popup__input_type_link-url");
+
 newCardForm.addEventListener("submit", function(event) {
 	event.preventDefault();
-	const nameInput = document.querySelector(
-		".popup_type_place .popup__input_type_name"
-	);
-	const imageInput = document.querySelector(
-		".popup_type_place .popup__input_type_link-url"
-	);
 
 	const newCard = {
 		name: nameInput.value,
@@ -64,6 +61,7 @@ newCardForm.addEventListener("submit", function(event) {
 
 	initialCards.unshift(newCard);
 	renderAllCards();
+	newCardForm.reset();
 	addPopup.close();
 });
 
