e702d74cdb89b926c01b81fbe7b8a9db709a3db6
Vadim Cherepanov <vadim.rgsu@yandex.ru>
2021-03-15 11:02:41 +0300

    ПРОЕКТ ЗАВЕРШЁН!

--- scripts/index.js
+++ scripts/index.js
@@ -24,6 +24,7 @@ const subtitleInput = document.querySelector("#input-popup-subtitle");
 const titleProfile = document.querySelector(".profile__title");
 const subtitleProfile = document.querySelector(".profile__subtitle");
 const editProfileForm = document.querySelector(".popup__form");
+const cards = document.querySelector(".cards");
 
 editButton.addEventListener("click", function() {
 	editPopup.open();
@@ -53,13 +54,9 @@ const imageInput = document.querySelector(".popup__input_type_link-url");
 newCardForm.addEventListener("submit", function(event) {
 	event.preventDefault();
 
-	const newCard = {
-		name: nameInput.value,
-		link: imageInput.value
-	};
+	const card = createCard(nameInput.value, imageInput.value);
+	cards.prepend(card);
 
-	initialCards.unshift(newCard);
-	renderAllCards();
 	newCardForm.reset();
 	addPopup.close();
 });
@@ -76,48 +73,32 @@ function openGallery(image, figcaption) {
 };
 
 // 4 отрисовка списка карточек
-
-function renderCard(card) {
-	return `<li class="card">
-	<img class="card__image" src="${card.link}" alt="${card.name}">
-	<div class="card__info">
-		<h2 class="card__title">${card.name}</h2>
-		<button type="button" class="card__like"></button>
-		<button type="button" class="card__delete-icon"></button>
-	</div>
-</li>`;
+function createCard(name, link) {
+	const card = document.createElement('li');
+	card.setAttribute('class', 'card');
+	card.innerHTML = `
+		<img class="card__image" src="${link}" alt="${name}">
+		<div class="card__info">
+			<h2 class="card__title">${name}</h2>
+			<button type="button" class="card__like"></button>
+			<button type="button" class="card__delete-icon"></button>
+		</div>
+	`;
+	card.addEventListener("click", function(event) {
+		if (event.target.classList.contains("card__like")) {
+			event.target.classList.toggle("card__like_active")
+		}
+		if (event.target.classList.contains("card__delete-icon")) {
+			cards.removeChild(event.currentTarget);
+		}
+		if (event.target.classList.contains("card__image")) {
+			openGallery(event.target.src, event.target.alt);
+		}
+	});
+	return card;
 }
 
-function renderAllCards() {
-	const cardHtml = initialCards
-		.map(function(card) {
-			return renderCard(card);
-		})
-		.join("\n");
-
-	document.querySelector(".cards").innerHTML = cardHtml;
-
-	let likes = document.querySelectorAll(".card__like");
-
-	likes.forEach(function(likeButton) {
-		likeButton.addEventListener("click", function() {
-			likeButton.classList.toggle("card__like_active");
-		});
-	});
-
-	let cards = document.querySelectorAll(".card");
-	let cardsContainer = document.querySelector(".cards");
-	cards.forEach(function(card) {
-		card.addEventListener("click", function(event) {
-			if (event.target.classList.contains("card__delete-icon")) {
-				cardsContainer.removeChild(event.currentTarget);
-			}
-
-			if (event.target.classList.contains("card__image")) {
-				openGallery(event.target.src, event.target.alt);
-			}
-		});
-	});
-}
-
-renderAllCards();
+initialCards.forEach((data) => {
+	const card = createCard(data.name, data.link);
+	cards.append(card);
+});
