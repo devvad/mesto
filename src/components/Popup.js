9d95c3e5954218b7c0815e3dc76fff9f9401715b
Vadim-Cherepanov-Web-Engineer <vadim.rgsu@yandex.ru>
2021-07-25 16:09:05 +0300

    Отладил Popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -4,14 +4,17 @@ export default class Popup {
 	constructor(popupSelector) {
 		this._rootElement = document.querySelector(popupSelector);
 		this._closeButtonElement = this._rootElement.querySelector(closeButtonSelector);
+		this._handleEscClose = this._handleEscClose.bind(this);
 	}
 
 	open() {
 		this._rootElement.classList.add(popupOpenClass);
+		document.addEventListener("keydown", this._handleEscClose);
 	}
 
 	close() {
 		this._rootElement.classList.remove(popupOpenClass);
+		document.removeEventListener("keydown", this._handleEscClose);
 	}
 
 	_handleEscClose(event) {
@@ -32,7 +35,6 @@ export default class Popup {
 
 	setEventListeners() {
 		this._rootElement.addEventListener("mousedown", (event) => this._handleOverlayClick(event));
-		document.addEventListener("keydown", (event) => this._handleEscClose(event));
 		this._closeButtonElement.addEventListener("click", (event) => this._handleCloseButtonClick(event));
 	}
 }
