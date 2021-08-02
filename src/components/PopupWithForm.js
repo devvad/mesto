9258b6f9c150439dd28e3e8c595f5d3f31b6f8a2
Vadim-Cherepanov-Web-Engineer <vadim.rgsu@yandex.ru>
2021-08-02 20:17:26 +0300

    Починил PopupWithForm

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -5,17 +5,16 @@ export default class PopupWithForm extends Popup {
 		this._handleFormSubmit = handleFormSubmit;
 		this._form = this._rootElement.querySelector("form");
 		this._popupButton = this._rootElement.querySelector(".popup__button");
+		this._inputs = this._rootElement.querySelectorAll(".popup__input");
 	}
 
 	setInputValues(formValues) {
-		const inputs = this._rootElement.querySelectorAll(".popup__input");
- 		inputs.forEach(input => input.value = formValues[input.name]);
+		this._inputs.forEach(input => input.value = formValues[input.name]);
 	}
 
 	_getInputValues() {
-		const inputs = this._rootElement.querySelectorAll(".popup__input");
 		const formValues = {};
- 		inputs.forEach(input => formValues[input.name] = input.value);
+		this._inputs.forEach(input => formValues[input.name] = input.value);
 		 return formValues;
 	}
 
@@ -23,7 +22,8 @@ export default class PopupWithForm extends Popup {
 		this._form.addEventListener("submit", (event) => {
 			const oldValue = this._popupButton.textContent;
 			this._popupButton.textContent = "Сохранение...";
-			this._handleFormSubmit(this._getInputValues()).then(() => {
+			this._handleFormSubmit(this._getInputValues())
+			.finally(() => {
 				this._popupButton.textContent = oldValue;
 			})
 			event.preventDefault();
